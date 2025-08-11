335079e9b5f17fd56dd11ca24ec32e943dffe3f9
Joris Belier <joris.belier@asml.com>
2025-08-11 13:24:05 +0200

    commit v1

--- src/matfrostjuliacall/matfrostjuliacall.cpp
+++ src/matfrostjuliacall/matfrostjuliacall.cpp
@@ -15,161 +15,162 @@ using matlab::mex::ArgumentList;
 #include <complex>
 
 #include <chrono>
-#include "converttojulia.hpp"
-#include "converttomatlab.hpp"
-
-extern "C" {
-    #include "matfrost.h"
-}
+// #include "converttojulia.hpp"
+// #include "converttomatlab.hpp"
+#include "main.cpp"
+//
+// extern "C" {
+//     #include "matfrost.h"
+// }
 
 
 
 class MexFunction : public matlab::mex::Function {
 private:
-    std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr;
-    ArgumentList* outputs_p;
-    ArgumentList* inputs_p;
-    std::thread juliaworkerthread;
-    
-    std::mutex mtx_mex;
-
-    std::mutex mtx_jl;
-    std::condition_variable cv_jl;
-
-    
-    bool                          exception_triggered = false;
-    matlab::engine::MATLABException exception_matlab;
-
-    MATFrostArray (*juliacall)(MATFrostArray) = nullptr;
-    void (*freematfrostmemory)(MATFrostArray) = nullptr;
+    // std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr;
+    // ArgumentList* outputs_p;
+    // ArgumentList* inputs_p;
+    // std::thread juliaworkerthread;
+    //
+    // std::mutex mtx_mex;
+    //
+    // std::mutex mtx_jl;
+    // std::condition_variable cv_jl;
+    //
+    //
+    // bool                          exception_triggered = false;
+    // matlab::engine::MATLABException exception_matlab;
+    //
+    // MATFrostArray (*juliacall)(MATFrostArray) = nullptr;
+    // void (*freematfrostmemory)(MATFrostArray) = nullptr;
     // std::string julia_environment_path;
 
 
 public:
     MexFunction() {
-        matlabPtr = getEngine(); 
-
-        std::unique_lock<std::mutex> lk(mtx_jl);
-
-        // Start Julia worker and initialize Julia.
-        juliaworkerthread = std::thread(&MexFunction::juliaworker, this);
-        
-        // wait for Julia init to finish.
-        cv_jl.wait(lk);
+        // matlabPtr = getEngine();
+        //
+        // std::unique_lock<std::mutex> lk(mtx_jl);
+        //
+        // // Start Julia worker and initialize Julia.
+        // juliaworkerthread = std::thread(&MexFunction::juliaworker, this);
+        //
+        // // wait for Julia init to finish.
+        // cv_jl.wait(lk);
 
     }
 
     virtual ~MexFunction(){
-        
-        matlab::data::ArrayFactory factory;
-
-        matlabPtr->feval(u"fprintf", 0, std::vector<matlab::data::Array>
-            ({ factory.createScalar("Closing MEX function")}));
+        //
+        // matlab::data::ArrayFactory factory;
+        //
+        // matlabPtr->feval(u"fprintf", 0, std::vector<matlab::data::Array>
+        //     ({ factory.createScalar("Closing MEX function")}));
     }
 
     void operator()(ArgumentList outputs, ArgumentList inputs) {
-        
-        // lk_mex will sequentialize Julia jobs in case of concurrent MEX-calls.
-        std::lock_guard<std::mutex> lk_mex(mtx_mex);
-        {
-            std::unique_lock<std::mutex> lk(mtx_jl);
-            inputs_p = &inputs;
-            outputs_p = &outputs;
-
-            // Start Julia worker Job
-            cv_jl.notify_one();
-            cv_jl.wait(lk);
-
-
-            if (exception_triggered){ 
-                exception_triggered = false;
-                throw exception_matlab;
-            } 
-        }
-        
+        main();
+        // // lk_mex will sequentialize Julia jobs in case of concurrent MEX-calls.
+        // std::lock_guard<std::mutex> lk_mex(mtx_mex);
+        // {
+        //     std::unique_lock<std::mutex> lk(mtx_jl);
+        //     inputs_p = &inputs;
+        //     outputs_p = &outputs;
+        //
+        //     // Start Julia worker Job
+        //     cv_jl.notify_one();
+        //     cv_jl.wait(lk);
+        //
+        //
+        //     if (exception_triggered){
+        //         exception_triggered = false;
+        //         throw exception_matlab;
+        //     }
+        // }
+        //
     }
 
-    int juliaworker(){
-        std::unique_lock<std::mutex> lk(mtx_jl);
-        jl_init();
-
-        void* matf                 = jl_eval_string("using MATFrost: MATFrost");
-        void* juliacall_c          = jl_eval_string("MATFrost.MEX.juliacall_c()");
-        void* freematfrostmemory_c = jl_eval_string("MATFrost.MEX.freematfrostmemory_c()");
-
-        if (matf != nullptr && juliacall_c != nullptr && freematfrostmemory_c != nullptr) {
-            juliacall          = (MATFrostArray (*)(MATFrostArray))  jl_unbox_voidpointer(juliacall_c);
-            freematfrostmemory = (void (*)(MATFrostArray))           jl_unbox_voidpointer(freematfrostmemory_c);
-        }
-
-        while(true){
-            cv_jl.notify_one();
-            cv_jl.wait(lk);
-
-            juliaworkerjob();
-        }
-        
-        return 0;
-    }
-
-
-    int juliaworkerjob() {
-        if (juliacall == nullptr || freematfrostmemory == nullptr) {
-            exception_triggered = true;
-            exception_matlab = matlab::engine::MATLABException(
-                "matfrostjulia:MATFrostPackageMissingInEnvironment",
-                u"Configurd Julia environment cannot load required MATFrost package. Either Julia environment is missing MATFrost dependency or environment has not been properly instantiated.");
-            return 0;
-        }
-        try
-        {
-            ArgumentList& outputs = *outputs_p;
-            ArgumentList& inputs = *inputs_p;
-            matlab::data::ArrayFactory factory;
-
-            const matlab::data::StructArray sarr = inputs[0];
-            const matlab::data::StringArray parr  = sarr[0]["package"];
-            std::string pack = matlab::engine::convertUTF16StringToUTF8String(parr[0]);
-            jl_eval_string(("import " + pack).c_str());
-
-            // During testing it occurred that function pointers became dangling. This issue occurred on Julia 1.8.
-            // Likely this is happening due to package import statement one line above. For safety we retrieve the
-            // pointer for every call.
-            juliacall          = (MATFrostArray (*)(MATFrostArray)) jl_unbox_voidpointer(jl_eval_string("MATFrost.MEX.juliacall_c()"));
-            freematfrostmemory = (void (*)(MATFrostArray))          jl_unbox_voidpointer(jl_eval_string("MATFrost.MEX.freematfrostmemory_c()"));
-
-            auto mfa = MATFrost::ConvertToJulia::convert(inputs[0]);
-
-            MATFrostArray   jlo = juliacall(mfa->matfrostarray);
-
-            const matlab::data::StructArray mato = MATFrost::ConvertToMATLAB::convert(jlo);
-
-            freematfrostmemory(jlo);
-
-            const matlab::data::TypedArray<bool> exception_b = mato[0]["exception"];
-            if (exception_b[0]) {
-                const matlab::data::StructArray matso = mato[0]["value"];
-                const matlab::data::StringArray exception_id = matso[0]["id"];
-                const matlab::data::StringArray exception_message = matso[0]["message"];
-                throw matlab::engine::MATLABException(exception_id[0], exception_message[0]);
-            }
-
-            outputs[0] = mato[0]["value"];
-
-        }
-        catch(const matlab::engine::MATLABException& ex)
-        {
-            exception_matlab = ex;
-            exception_triggered = true;
-        }
-        catch(std::exception& ex)
-        {
-            matlab::data::ArrayFactory factory;
-            matlabPtr->feval(u"disp", 0, std::vector<matlab::data::Array>
-                ({ factory.createScalar("###################################\nMATFrost error: " + std::string(ex.what()) + "\n###################################\n")}));
-        }
-        return 0; 
-    }
+    // int juliaworker(){
+    //     std::unique_lock<std::mutex> lk(mtx_jl);
+    //     jl_init();
+    //
+    //     void* matf                 = jl_eval_string("using MATFrost: MATFrost");
+    //     void* juliacall_c          = jl_eval_string("MATFrost.MEX.juliacall_c()");
+    //     void* freematfrostmemory_c = jl_eval_string("MATFrost.MEX.freematfrostmemory_c()");
+    //
+    //     if (matf != nullptr && juliacall_c != nullptr && freematfrostmemory_c != nullptr) {
+    //         juliacall          = (MATFrostArray (*)(MATFrostArray))  jl_unbox_voidpointer(juliacall_c);
+    //         freematfrostmemory = (void (*)(MATFrostArray))           jl_unbox_voidpointer(freematfrostmemory_c);
+    //     }
+    //
+    //     while(true){
+    //         cv_jl.notify_one();
+    //         cv_jl.wait(lk);
+    //
+    //         juliaworkerjob();
+    //     }
+    //
+    //     return 0;
+    // }
+    //
+    //
+    // int juliaworkerjob() {
+    //     if (juliacall == nullptr || freematfrostmemory == nullptr) {
+    //         exception_triggered = true;
+    //         exception_matlab = matlab::engine::MATLABException(
+    //             "matfrostjulia:MATFrostPackageMissingInEnvironment",
+    //             u"Configurd Julia environment cannot load required MATFrost package. Either Julia environment is missing MATFrost dependency or environment has not been properly instantiated.");
+    //         return 0;
+    //     }
+    //     try
+    //     {
+    //         ArgumentList& outputs = *outputs_p;
+    //         ArgumentList& inputs = *inputs_p;
+    //         matlab::data::ArrayFactory factory;
+    //
+    //         const matlab::data::StructArray sarr = inputs[0];
+    //         const matlab::data::StringArray parr  = sarr[0]["package"];
+    //         std::string pack = matlab::engine::convertUTF16StringToUTF8String(parr[0]);
+    //         jl_eval_string(("import " + pack).c_str());
+    //
+    //         // During testing it occurred that function pointers became dangling. This issue occurred on Julia 1.8.
+    //         // Likely this is happening due to package import statement one line above. For safety we retrieve the
+    //         // pointer for every call.
+    //         juliacall          = (MATFrostArray (*)(MATFrostArray)) jl_unbox_voidpointer(jl_eval_string("MATFrost.MEX.juliacall_c()"));
+    //         freematfrostmemory = (void (*)(MATFrostArray))          jl_unbox_voidpointer(jl_eval_string("MATFrost.MEX.freematfrostmemory_c()"));
+    //
+    //         auto mfa = MATFrost::ConvertToJulia::convert(inputs[0]);
+    //
+    //         MATFrostArray   jlo = juliacall(mfa->matfrostarray);
+    //
+    //         const matlab::data::StructArray mato = MATFrost::ConvertToMATLAB::convert(jlo);
+    //
+    //         freematfrostmemory(jlo);
+    //
+    //         const matlab::data::TypedArray<bool> exception_b = mato[0]["exception"];
+    //         if (exception_b[0]) {
+    //             const matlab::data::StructArray matso = mato[0]["value"];
+    //             const matlab::data::StringArray exception_id = matso[0]["id"];
+    //             const matlab::data::StringArray exception_message = matso[0]["message"];
+    //             throw matlab::engine::MATLABException(exception_id[0], exception_message[0]);
+    //         }
+    //
+    //         outputs[0] = mato[0]["value"];
+    //
+    //     }
+    //     catch(const matlab::engine::MATLABException& ex)
+    //     {
+    //         exception_matlab = ex;
+    //         exception_triggered = true;
+    //     }
+    //     catch(std::exception& ex)
+    //     {
+    //         matlab::data::ArrayFactory factory;
+    //         matlabPtr->feval(u"disp", 0, std::vector<matlab::data::Array>
+    //             ({ factory.createScalar("###################################\nMATFrost error: " + std::string(ex.what()) + "\n###################################\n")}));
+    //     }
+    //     return 0;
+    // }
 
 
     
---
+++ src/matfrostjuliacall/matfrostjuliaserver.cpp
@@ -0,0 +1,227 @@
+/**
+ * This file is responsible of managing the Julia process and offer a communication interface over pipes with the Julia
+ * process. This class is free of MATLAB dependencies
+ */
+#include <cstdint>
+#include <windows.h>
+#include <tchar.h>
+#include <cstdio>
+#include <strsafe.h>
+
+
+#include <string>
+#include <iostream>
+#include <array>
+
+#define BUFSIZE 16384
+
+class JuliaProcess {
+
+    struct {
+        std::array<uint8_t, BUFSIZE> buffer;
+        size_t position;
+        size_t available;
+    } outputstream = {.buffer ={}, .position = 0, .available = 0};
+
+    struct {
+        std::array<uint8_t, BUFSIZE> buffer{};
+        size_t position = 0;
+        size_t available = 0;
+    } inputstream = {.buffer ={}, .position = 0, .available = 0};
+
+
+    std::array<HANDLE, 2> h_stdin;
+    std::array<HANDLE, 2> h_stdout;
+    std::array<HANDLE, 2> h_stderr;
+
+    // STD_INPUT_HANDLE
+
+    PROCESS_INFORMATION process_information;
+
+    JuliaProcess(std::array<HANDLE, 2> h_stdin, std::array<HANDLE, 2> h_stdout, std::array<HANDLE, 2> h_stderr, PROCESS_INFORMATION process_information) :
+        h_stdin(h_stdin), h_stdout(h_stdout), h_stderr(h_stderr), process_information(process_information)
+    {
+
+
+    }
+
+public:
+
+    void write(const uint8_t* buf, const size_t nb) {
+        size_t bw = std::min(outputstream.buffer.size() - outputstream.available, nb);
+        memcpy(&outputstream.buffer[outputstream.available], buf, bw);
+        outputstream.available += bw;
+
+        if (bw >= nb) {
+            return;
+        }
+
+        flush();
+
+        while (nb - bw >= outputstream.buffer.size()) {
+            DWORD bytes_written = 0;
+            WriteFile(
+                h_stdin[1],
+                &buf[bw],
+                outputstream.buffer.size(),
+                &bytes_written,
+                nullptr);
+            bw += bytes_written;
+        }
+
+        if (bw < nb) {
+            outputstream.position  = 0;
+            outputstream.available = nb - bw;
+            memcpy(&outputstream.buffer[0], &buf[bw], outputstream.available);
+        }
+    }
+
+    void flush() {
+        while (outputstream.available > outputstream.position) {
+            DWORD bytes_written = 0;
+            WriteFile(
+                h_stdin[1],
+                &outputstream.buffer[outputstream.position],
+                outputstream.available-outputstream.position,
+                &bytes_written,
+                nullptr);
+
+            outputstream.position += bytes_written;
+        }
+        outputstream.position = 0;
+        outputstream.available = 0;
+
+
+    }
+
+
+    void read(uint8_t* buf, const size_t nb) {
+        size_t br = 0;
+
+        while (br < nb) {
+            if (inputstream.available - inputstream.position > 0) {
+                size_t brn = std::min(inputstream.available - inputstream.position , nb - br);
+                memcpy(&buf[br], &inputstream.buffer[inputstream.position], brn);
+                inputstream.position += brn;
+                br += brn;
+
+            } else if (nb - br >= inputstream.buffer.size()) {
+                DWORD bytes_read;
+                ReadFile(
+                    h_stdout[0],
+                    &buf[br],
+                    inputstream.buffer.size(),
+                    &bytes_read,
+                    nullptr);
+                br += bytes_read;
+            } else {
+                DWORD bytes_read;
+
+                ReadFile(
+                    h_stdout[0],
+                    &inputstream.buffer[0],
+                    inputstream.buffer.size(),
+                    &bytes_read,
+                    nullptr);
+
+                inputstream.position = 0;
+                inputstream.available = bytes_read;
+
+            }
+
+
+        }
+
+    };
+
+
+    static JuliaProcess spawn(const std::string& bin, const std::string& project) {
+        // TCHAR szCmdline[]=TEXT("C:\\Users\\jbelier\\.julia\\juliaup\\julia-1.12.0-rc1+0.x64.w64.mingw32\\bin\\julia.exe -e \"println(read(stdin, Int64))\"");
+
+        std::string cmdline = "\"" + bin + "\" --project=\"" + project + "\" -e \"using MATFrostDev ; MATFrostDev.serve()\"";//read(stdin, Int64))\"";
+
+        SECURITY_ATTRIBUTES saAttr;
+
+        printf("\n->Start of parent execution.\n");
+
+        // Set the bInheritHandle flag so pipe handles are inherited.
+
+
+        saAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
+        saAttr.bInheritHandle = TRUE;
+        saAttr.lpSecurityDescriptor = NULL;
+
+        std::array<HANDLE, 2> h_stdin{};
+        std::array<HANDLE, 2> h_stdout{};
+        std::array<HANDLE, 2> h_stderr{};
+
+        if ( ! CreatePipe(&h_stdin[0], &h_stdin[1], &saAttr, 16384*4) ) {
+            // ErrorExit(TEXT("StdoutRd CreatePipe"));
+        }
+        if ( ! SetHandleInformation(&h_stdin[1], HANDLE_FLAG_INHERIT, 0) ) {
+            // ErrorExit(TEXT("Stdout SetHandleInformation"));
+        }
+
+        if ( ! CreatePipe(&h_stdout[0], &h_stdout[1], &saAttr, 16384*4) ) {
+            // ErrorExit(TEXT("StdoutRd CreatePipe"));
+        }
+        if ( ! SetHandleInformation(&h_stdout[0], HANDLE_FLAG_INHERIT, 0) ) {
+            // ErrorExit(TEXT("Stdout SetHandleInformation"));
+        }
+
+        if ( ! CreatePipe(&h_stderr[0], &h_stderr[1], &saAttr, 16384*4) ) {
+            // ErrorExit(TEXT("StdoutRd CreatePipe"));
+        }
+        if ( ! SetHandleInformation(&h_stderr[0], HANDLE_FLAG_INHERIT, 0) ) {
+            // ErrorExit(TEXT("Stdout SetHandleInformation"));
+        }
+
+        std::cout << "HANDLE stdin.Read: " << h_stdin[0] << " stdin.Write:" << h_stdin[1] << std::endl;
+
+
+
+
+        PROCESS_INFORMATION piProcInfo;
+        STARTUPINFO siStartInfo;
+        ZeroMemory( &piProcInfo, sizeof(PROCESS_INFORMATION) );
+        ZeroMemory( &siStartInfo, sizeof(STARTUPINFO) );
+
+
+        // Set up members of the PROCESS_INFORMATION structure.
+
+
+        siStartInfo.cb = sizeof(STARTUPINFO);
+        siStartInfo.hStdInput  = h_stdin[0];
+        siStartInfo.hStdOutput = h_stdout[1];
+        siStartInfo.hStdError  = h_stderr[1];
+        siStartInfo.dwFlags |= STARTF_USESTDHANDLES;
+
+        // Create the child process.
+
+        CreateProcessA(
+          bin.c_str(),
+          &cmdline[0],   // command line
+          nullptr,       // process security attributes
+          nullptr,       // primary thread security attributes
+          TRUE,          // handles are inherited
+          0,             // creation flags
+          nullptr,       // use parent's environment
+          nullptr,       // use parent's current directory
+          &siStartInfo,  // STARTUPINFO pointer
+          &piProcInfo);  // receives PROCESS_INFORMATION
+
+
+        CloseHandle(h_stdin[0]);
+        CloseHandle(h_stdout[1]);
+        CloseHandle(h_stderr[1]);
+
+
+        const JuliaProcess jp(h_stdin, h_stdout, h_stderr, piProcInfo);
+
+        return jp;
+
+    }
+
+
+
+};
