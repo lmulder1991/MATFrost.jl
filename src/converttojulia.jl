17b0a074b7677f6bbd5d2070889b9f3fef01b004
Joris Belier <joris.belier@asml.com>
2025-08-16 21:54:27 +0200

    Update converttojulia.jl

--- src/converttojulia.jl
+++ src/converttojulia.jl
@@ -453,6 +453,62 @@ end
 end
 
 
+@generated function read_matlab!(io::BufferedStream, ::Type{Array{T,N}}) where {T,N}
+    
+    return quote
+        dims = read_matfrostarray_header!(io, STRUCT, Val{N}())
+        numfields_mat = read!(io, Int64)
+        fieldnames_mat = Vector{Symbol}(undef, numfields_mat)
+        fieldname_in_type = Vector{Bool}(undef, numfields_mat)
+        for i in eachindex(fieldnames_mat)
+            fieldnames_mat[i] = Symbol(read_string!(io))
+            fieldname_in_type[i] = fieldnames_mat[i] in fieldnames(T)
+        end
+        
+        if (numfields_mat != length(fieldnames(T)) || !all(fieldname_in_type))
+            clear_matfrost_object!(io, numfields_mat*prod(dims))
+            throw("Fieldnames do not match")
+        end
+
+        arr = Array{T,N}(undef, dims)
+        
+        for eli in eachindex(arr)
+
+            # Create local variables with type annotation, {Nothing, FieldType}
+            $((quote
+                $(Symbol(:_lfv_, fieldnames(T)[i])) :: Union{Nothing, $(fieldtypes(T)[i])} = nothing
+            end for i in eachindex(fieldnames(T)))...)
+
+            # Parse each field value. Parsing must be done in the order of MATFrostSequence
+            for fn_i in 1:length(fieldnames_mat)
+                fieldname = fieldnames_mat[fn_i]
+                try 
+                    $((quote
+                        if (fieldname == fieldnames(T)[$(i)])
+                            $(Symbol(:_lfv_, fieldnames(T)[i])) = read_matlab!(io, $(fieldtypes(T)[i]))
+                        end
+                    end for i in eachindex(fieldnames(T)))...)
+                catch e
+                    clear_matfrost_object!(io, (prod(dims)-eli+1)*numfields_mat - fn_i)
+                    throw(e)
+                end
+            end
+
+            # Force {Nothing, FieldType} to FieldType
+            $((quote
+                $(Symbol(:_lfva_, fieldnames(T)[i])) :: $(fieldtypes(T)[i]) = $(Symbol(:_lfv_, fieldnames(T)[i]))
+            end for i in eachindex(fieldnames(T)))...)
+
+            # Construct new struct
+            arr[eli] = T($((Symbol(:_lfva_, fieldnames(T)[i]) for i in eachindex(fieldnames(T)))...))
+
+        end
+        arr
+    end
+
+end
+
+
 #  instream= MATFrost._Stream.BufferedStream(0, Vector{UInt8}(undef, 2<<13), 0, 0)
 #   MATFrost._ConvertToJulia.read_matlab!(instream, MATFrost.StructTest)
 
