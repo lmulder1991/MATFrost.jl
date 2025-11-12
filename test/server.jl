4bf58031e69edf46ec48e15bf3183db1e53448da
lmulder <lennard.mulder@asml.com>
2025-11-12 09:39:24 +0100

    updated unitTests

--- test/server.jl
+++ test/server.jl
@@ -18,22 +18,30 @@ end
     callMeta = MATFrost._Server.CallMeta("MATFrost._Server.getMethod")
     (f,m) = MATFrost._Server.getMethod(callMeta)
     @test isa(f, Function)
-    @test isa(m, Method)
+    @test m==Tuple{MATFrost._Server.CallMeta}
 
     # Test: function with multiple methods should throw ambiguity error
     callMeta = MATFrost._Server.CallMeta("MATFrost._ConvertToJulia.convert_matfrostarray")
     @test_throws MATFrost._Types.MATFrostException MATFrost._Server.getMethod(callMeta)
+    err = nothing
+    try
+        MATFrost._Server.getMethod(callMeta)
+    catch e
+        err = e
+    end
+    @test occursin("Ambiguous function call", err.message)
+    @test err.id == "matfrostjulia:call:multipleMethodDefinitions"
 
     # Test: lower level function with many methods, specific signature
     callMeta = MATFrost._Server.CallMeta("MATFrost._ConvertToJulia.convert_matfrostarray","::Type{String}, marr::MATFrost._Types.MATFrostArrayAbstract")
     (f,m) = MATFrost._Server.getMethod(callMeta)
     @test isa(f, Function)
-    @test isa(m, Method)
+    @test m==Tuple{Type{String}, MATFrost._Types.MATFrostArrayAbstract}
 
     # Test: non-existing function should throw error
     callMeta = MATFrost._Server.CallMeta("MATFrost.nonExistentFunction")
     @test_throws ErrorException MATFrost._Server.getMethod(callMeta)
-        try
+    try
         MATFrost._Server.getMethod(callMeta)
     catch e
         @test occursin("Function MATFrost.nonExistentFunction not found", e.msg)
